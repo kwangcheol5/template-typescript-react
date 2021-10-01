00d788605520bf86f5163e944174e389f8478b29
김광철 <anyman@zigbang.com>
2021-10-01 08:26:13 +0000

    테스트

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 안녕하세요
         </a>
       </header>
     </div>
